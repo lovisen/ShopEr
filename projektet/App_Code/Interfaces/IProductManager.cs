353318dc918ff6df34083043d3c4eeb87c4b425e
lovisen <louize_e@hotmail.com>
2009-10-20 11:24:55 +0200

    yjok
    Signed-off-by: lovisen <louize_e@hotmail.com>

---
+++ projektet/App_Code/Interfaces/IProductManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Collections.Generic;
+
+/// <summary>
+/// Interface for ProductManagers
+/// </summary>
+public interface IProductManager
+{
+     List<ProductItem> GetAllProducts();
+     ProductItem GetProductById(int id);
+     List<ProductItem> SearchProducts();
+    void GetAllFeaturedProducts();
+    void InsertProduct();
+    void UpdateProduct();
+
+   }
---
+++ projektet/App_Code/Managers/ProductManagerByLINQ.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+/// <summary>
+/// Summary description for ProductManagerByLINQ
+/// </summary>
+public class ProductManagerByLINQ : IProductManager
+{
+	public ProductManagerByLINQ()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+
+
+
+    #region IProductManager Members
+
+    public System.Collections.Generic.List<ProductItem> GetAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ProductItem GetProductById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public System.Collections.Generic.List<ProductItem> SearchProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void GetAllFeaturedProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void InsertProduct()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateProduct()
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+}
