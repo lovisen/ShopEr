8c119d0a5e5ed7ef486acab9f9765a4922f7d04d
Tony <tav85@hotmail.com>
2009-11-02 09:21:16 +0100

    Går nu att anmäla sig till nyhetsbrev. Tabell Subscriber måste finnas i databas med kolumn Id och Email.

---
+++ projektet/App_Code/Managers/SubscriberManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Data.Common;
+
+/// <summary>
+/// Summary description for SubscriberManager
+/// </summary>
+public class SubscriberManager
+{
+
+    public SubscriberManager()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public static bool InsertSubscriber(SubscriberItem n)
+    {
+        try
+        {
+            DbCommand comm = DataAccess.CreateCommand();
+            comm.CommandText = "INSERT INTO Subscriber(Email) VALUES(@Email)";
+            comm.CreateAndAddParameter("@Email", n.Email, DbType.String);
+
+            DataAccess.ExecuteNonQuery(comm);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
